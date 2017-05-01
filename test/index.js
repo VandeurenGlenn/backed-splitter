b2c3d54518409cf29e9dc5516c1b2f5b6a934c2d
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-05-01 21:34:34 +0200

    :1234: Update test

--- test/index.js
+++ test/index.js
@@ -5,7 +5,9 @@ const splitter = require('./../lib/splitter-node.js');
 describe('backed-splitter test suite', () => {
   it('test defaults (ignores duplicates, imports in imports)', done => {
     splitter({entry: 'test/templates/index.html'}).then(result => {
-      assert.equal(Object.keys(result.scripts).length, 2);
+      assert.equal(result.bundleHref, 'imported-app.html');
+      assert.equal(Object.keys(result.imports).length, 4);
+      assert.equal(Object.keys(result.scripts).length, 4);
       done();
     })
   });
