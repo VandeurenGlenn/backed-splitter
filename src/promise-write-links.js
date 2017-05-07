a3b8a4b44ecbef003a28b34add999ddee66e00ce
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-05-07 21:05:58 +0200

    :unamused: Remove unexpected semicolon closes #6

--- src/promise-write-links.js
+++ src/promise-write-links.js
@@ -16,7 +16,7 @@ export default (source, href, js, css) => {
   let head = dom5.query(new HTMLElement5(source).value, rules);
   if (css) source = source.replace('</head>', `  <link rel="stylesheet" href="${href.replace('.html', '.css')}">
   </head>`);
-  source = source.replace('</head>', `  <link rel="import" href="${href}">;
+  source = source.replace('</head>', `  <link rel="import" href="${href}">
   </head>`);
   if (js) source = source.replace('</body>', `  <script src="${href.replace('.html', '.js')}"></script>
   </body>`);
