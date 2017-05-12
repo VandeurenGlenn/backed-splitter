4addfb97ab00f42562350f2beba0daa0ae75de07
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-05-12 18:41:08 +0200

    :mag: Update test

--- test/index.js
+++ test/index.js
@@ -4,8 +4,9 @@ const splitter = require('./../lib/splitter-node.js');
 
 describe('backed-splitter test suite', () => {
   it('test defaults (ignores duplicates, imports in imports)', done => {
-    splitter({entry: 'test/templates/index.html', exclude: ['bower_components/**/*', '**/*.css']}).then(result => {
+    splitter({entry: 'test/templates/index.html', exclude: ['**/*.css', '*.json'], external: ['some.js', 'bower_components/**/*']}).then(result => {
       assert.equal(result.bundleHref, 'imported-app.html');
+      assert.equal(Object.keys(result.external).length, 2);
       assert.equal(Object.keys(result.imports).length, 4);
       assert.equal(Object.keys(result.scripts).length, 4);
       done();
